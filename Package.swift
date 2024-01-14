1425eb0dd322f93e0d85d81af34905d7a0845c06
xuty <xty50337@hotmail.com>
2024-01-14 11:40:08 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -155,7 +155,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SDL",
+            name: "SwiftSDL2",
             targets: ["SwiftSDL2"]
         )
     ],
